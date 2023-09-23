c250a0935fba65e84cdf969eee77851354b76ef5
lwalker <landen@example.com>
2023-09-23 14:11:41 -0500

    incomplete, but mostly done thought and user models + general file layout + server.js

---
+++ Develop/models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+// Schema to create Post model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 15,
+        maxLength: 500,
+      },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `getTags` that gets the amount of tags associated with an application
+
+// Initialize our Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ Develop/models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
