16735d0b957dfc1c1a706f9be75c08365030d17c
lwalker <landen@example.com>
2023-09-26 16:39:38 -0500

    updated api route for user to include updateSingleUser and deleteSingleUser

--- Develop/routes/api/userRoutes.js
+++ Develop/routes/api/userRoutes.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
+
 const {
   getUsers,
   getSingleUser,
   createUser,
+  updateSingleUser,
+  deleteSinglueUser,
 } = require('../../controllers/usersController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).put(updateSingleUser).delete(deleteSingleUser);
 
 module.exports = router;
