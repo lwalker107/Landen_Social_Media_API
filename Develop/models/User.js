3754ff53b16d0959c192600584dce7cf86e831f3
lwalker <landen@example.com>
2023-09-26 16:18:00 -0500

    added validator for email and getters for user.js model file

--- Develop/models/User.js
+++ Develop/models/User.js
@@ -3,9 +3,25 @@ const { Schema, model } = require('mongoose');
 // Schema to create User model
 const userSchema = new Schema(
   {
-    username: { type: String, required: true, unique: true, trim: true },
-    /* need a validator for email */
-    email: { type: String, required: true, unique: true },
+    username: { 
+      type: String, 
+      required: true, 
+      unique: true, 
+      trim: true 
+    },
+    email: { 
+      type: String, 
+      required: true, 
+      unique: true,
+      validate: {
+        validator: function(v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Email must be valid" 
+      },
+      required: [true, "Email required"]
+    },
+    
     thoughts: [
       {
         type: Schema.Types.ObjectId,
@@ -20,10 +36,10 @@ const userSchema = new Schema(
       ],
   },
   {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
