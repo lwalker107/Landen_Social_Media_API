be1dfe7fe37e7b8fb9fef5a98740f8bbff7d8e9e
lwalker <landen@example.com>
2023-09-26 16:02:03 -0500

    added updateUser function to usersController

--- Develop/controllers/usersController.js
+++ Develop/controllers/usersController.js
@@ -34,7 +34,25 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // Delete a user and associated apps
+  // Update a user by ID
+  async updatedUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        {_id: req.params.userId},
+        { $set: req.body},
+        { runValidators: true, new: true });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID found'});
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // Delete a user and associated thoughts using ID
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndDelete({ _id: req.params.userId });
